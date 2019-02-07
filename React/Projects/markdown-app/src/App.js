7f94d8a7ee6826a12e6784fa3f1810998fdadcd9
David LAPORT <david.laport@live.fr>
2019-02-07 10:28:25 +0100

    add project training 1

--- React/Projects/markdown-app/src/App.js
+++ React/Projects/markdown-app/src/App.js
@@ -1,30 +1,35 @@
-import React, { Component } from 'react'
-import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 import marked from 'marked'
 
 import { sampleText } from './sampleText'
 
 class App extends Component {
+
   state = {
     text: sampleText
   }
 
-  componentDidMount () {
+  componentDidMount () { //Chargé à l'initialisation du state
+    console.log('Je suis monté.')
     const text = localStorage.getItem('text')
-
-    if (text) {
+    if(text){
       this.setState({ text })
-    } else {
+    } else{
       this.setState({ text: sampleText })
     }
   }
 
-  componentDidUpdate () {
+  componentDidUpdate () { //Chargé à chaque mise à jour du state
+    console.log('Je me met à jour.')
     const { text } = this.state
     localStorage.setItem('text', text)
   }
-
+    
+  
   handleChange = event => {
     const text = event.target.value
     this.setState({ text })
@@ -34,25 +39,28 @@ class App extends Component {
     const __html = marked(text, { sanitize: true })
     return { __html }
   }
-
-  render () {
+  
+  
+  render() {
     return (
-      <div className='container'>
-        <div className='row'>
-          <div className='col-sm-6'>
-            <textarea
-              onChange={this.handleChange}
-              value={this.state.text}
-              className='form-control'
-              rows='35' />
+      <div className="container">
+        <div className="row">
+          <div className=".col-sm-6">
+            <textarea 
+            onChange={this.handleChange}
+            className='form-control' 
+            rows="35" 
+            value={this.state.text}>
+
+            </textarea>
           </div>
-          <div className='col-sm-6'>
+          <div className=".col-sm-6">
             <div dangerouslySetInnerHTML={this.renderText(this.state.text)} />
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
