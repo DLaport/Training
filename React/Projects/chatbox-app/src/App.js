c0ca47ca11cb06ad462ef0d8d93c5f581555697f
David LAPORT <david.laport@live.fr>
2019-02-08 17:50:18 +0100

    add again

--- React/Projects/chatbox-app/src/App.js
+++ React/Projects/chatbox-app/src/App.js
@@ -4,6 +4,18 @@ import Formulaire from './components/Formulaire'
 import { Message } from './components/Message';
 
 class App extends Component {
+
+  state = {
+    messages: {},
+    pseudo: this.props.match.params.pseudo
+  }
+
+  addMessage = message => {
+    const messages = { ...this.state.messages }
+    messages[`message-${Date.now()}`] = message
+    this.setState({ messages })
+  }
+
   render () {
     return (
       <div className='box'>
@@ -14,7 +26,9 @@ class App extends Component {
           <Message />
         </div>
 
-        <Formulaire />
+        <Formulaire 
+          pseudo={ this.state.pseudo }
+          addMessage={ this.addMessage } />
       </div>
     )
   }
--- React/Projects/chatbox-app/src/components/Formulaire.js
+++ React/Projects/chatbox-app/src/components/Formulaire.js
@@ -1,18 +1,48 @@
 import React, { Component } from 'react'
 
 export default class Formulaire extends Component {
-  render() {
-    return (
-        <form 
-            className='form'>
-        <textarea 
-            maxLength='140' 
-            required />
-            <div className='info'>
-                140
-            </div>
-        <button type='submit'>Envoyer !</button>
-    </form>
-    )
-  }
+    state = {
+        message: ''
+    }
+
+    handleSubmit = event => {
+        event.preventDefault()
+        this.createMessage()
+    }
+
+    handleChange = event => {
+        const message = event.target.value
+        this.setState({ message })
+    }
+
+    createMessage = () => {
+        const { addMessage, pseudo } = this.props
+
+        const message = {
+            pseudo,
+            message: this.state.message
+        }
+        addMessage(message)
+
+        //Reset
+        this.setState({ message: '' })
+    }
+
+    render() {
+        return (
+            <form 
+                className='form'
+                onSubmit={ this.handleSubmit }>
+            <textarea 
+                maxLength='140' 
+                required 
+                onChange={ this.handleChange }
+                value={ this.state.message }/>
+                <div className='info'>
+                    140
+                </div>
+            <button type='submit'>Envoyer !</button>
+        </form>
+        )
+    }
 }
